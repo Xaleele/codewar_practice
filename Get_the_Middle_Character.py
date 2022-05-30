dc59198708083939ef5a9939c1f920dd2523df20
Alex <alexylee415@gmail.com>
2022-05-29 17:15:25 -0700

    tested list logic

---
+++ Get_the_Middle_Character.py
@@ -0,0 +1,11 @@
+s = 'test'
+empty_list = []
+
+for letter in s:
+    empty_list.append(letter)
+if len(empty_list) % 2 == 0:
+    print('Even')
+else:
+    print('Odd')
+def get_middle(s):
+    pass
