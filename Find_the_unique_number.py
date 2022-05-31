2302f44a5a9a0f7d14ab94cea2442fefae2d2320
Alex <alexylee415@gmail.com>
2022-05-31 15:55:23 -0700

    defined parameters for solution.

---
+++ Find_the_unique_number.py
@@ -0,0 +1,12 @@
+def find_uniq(arr):
+    for n in range(len(arr)):
+        if arr[n] == arr[n+1]:
+            continue
+        elif arr[n] != arr[n+1] and arr[n] == arr[n-1]:
+            print(arr[n+1])
+            break
+
+
+
+
+
