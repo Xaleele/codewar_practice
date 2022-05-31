afe951b1c1a334c72a671692a95a46d2480305e6
Alex <alexylee415@gmail.com>
2022-05-30 18:20:10 -0700

    switched round for int to round down numbers

--- Get_the_Middle_Character.py
+++ Get_the_Middle_Character.py
@@ -1,14 +1,12 @@
-s = 'test'
-empty_list = []
-
-for letter in s:
-    empty_list.append(letter)
-if len(empty_list) == 1:
-    print(empty_list[0])
-
-elif len(empty_list) % 2 != 0:
-    print(str(empty_list[round(len(empty_list)/2)]))
-else:
-    print(str(empty_list[round(len(empty_list)/2)-1])+str(empty_list[round(len(empty_list)/2)]))
 def get_middle(s):
-    pass
+    empty_list = []
+
+    for letter in s:
+        empty_list.append(letter)
+    if len(empty_list) == 1:
+        return (empty_list[0])
+
+    elif len(empty_list) % 2 != 0:
+        return (str(empty_list[int(len(empty_list) / 2)]))
+    else:
+        return (str(empty_list[int(len(empty_list) / 2) - 1]) + str(empty_list[int(len(empty_list) / 2)]))
