42e10cd20b6da9b6f4aee753988c4729a7270495
Alex <alexylee415@gmail.com>
2022-05-30 18:13:04 -0700

    added scenario for even length

--- Get_the_Middle_Character.py
+++ Get_the_Middle_Character.py
@@ -1,4 +1,4 @@
-s = 'e'
+s = 'test'
 empty_list = []
 
 for letter in s:
@@ -7,8 +7,8 @@ if len(empty_list) == 1:
     print(empty_list[0])
 
 elif len(empty_list) % 2 != 0:
-    print(empty_list[round(len(empty_list)/2)])
+    print(str(empty_list[round(len(empty_list)/2)]))
 else:
-    print('Even')
+    print(str(empty_list[round(len(empty_list)/2)-1])+str(empty_list[round(len(empty_list)/2)]))
 def get_middle(s):
     pass
