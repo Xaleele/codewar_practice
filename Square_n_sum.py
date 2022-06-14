f5f67c286dc6ae23ddc6926f09e6037ab3e3e183
Alex <alexylee415@gmail.com>
2022-06-13 21:02:13 -0700

    Defined solution for square_n_sum challenge.

--- Square_n_sum.py
+++ Square_n_sum.py
@@ -1,2 +1,6 @@
-def square_sum(number):
-    pass
+def square_sum(numbers):
+    empty_list = []
+    for num in range(len(numbers)):
+        numsq = numbers[num]**2
+        empty_list.append(numsq)
+    return sum(empty_list)
