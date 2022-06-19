e114e1cb819a220edd60a9540cdc568d75e2e9cb
Alex <alexylee415@gmail.com>
2022-06-18 23:02:19 -0700

    Added second logic: if the string contained numbers.

--- String_incrementer.py
+++ String_incrementer.py
@@ -1,6 +1,17 @@
 def increment_string(strng):
+    n_list = []
+    s_list = []
     if strng.isalpha():
         strng = strng + "1"
         return strng
     else:
-        pass
+        for i in range(len(strng)):
+            if strng[i].isnumeric():
+                n_list.append(strng[i])
+            else:
+                s_list.append(strng[i])
+        n_list = ''.join(n_list)
+        s_list = ''.join(s_list)
+        i_list = int(n_list) + 1
+        strng = s_list + str(i_list)
+        return strng
