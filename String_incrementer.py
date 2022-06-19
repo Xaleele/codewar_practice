15d3b75dfc76e0a9c661a8fffd4993036d682341
Alex <alexylee415@gmail.com>
2022-06-18 23:38:54 -0700

    Added third logic: if the string contained leading zeroes.

--- String_incrementer.py
+++ String_incrementer.py
@@ -1,4 +1,5 @@
 def increment_string(strng):
+    new_i = ''
     n_list = []
     s_list = []
     if strng.isalpha():
@@ -10,8 +11,15 @@ def increment_string(strng):
                 n_list.append(strng[i])
             else:
                 s_list.append(strng[i])
-        n_list = ''.join(n_list)
-        s_list = ''.join(s_list)
-        i_list = int(n_list) + 1
-        strng = s_list + str(i_list)
-        return strng
+        n = ''.join(n_list)
+        s = ''.join(s_list)
+        iw = int(n) + 1
+        #print(iw)
+        zero_lead = ("0" * (len(n) - len(str(iw)))) + str(iw)
+        return s + zero_lead
+
+
+
+
+increment_string('')
+
