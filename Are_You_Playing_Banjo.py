f48acc8446845bd9fcc6679b792cf9832dcecd3a
Alex <alexylee415@gmail.com>
2022-07-22 21:42:44 -0700

    Create simple if else statement with equals, or operators.

--- Are_You_Playing_Banjo.py
+++ Are_You_Playing_Banjo.py
@@ -1,3 +1,5 @@
 def are_you_playing_banjo(name):
-    # Implement me!
-    return name
+    if name[0] == 'r' or name[0] == 'R':
+        print(name + ' plays banjo')
+    else:
+        print(name + ' does not play banjo')
